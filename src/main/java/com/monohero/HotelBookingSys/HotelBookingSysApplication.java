a73944dab5d4df1cb9761c93d55fed7435f2e449
monoheroshiraf <shirafsf@gmail.com>
2023-12-04 04:32:13 +0530

    Initial commit

---
+++ src/main/java/com/monohero/HotelBookingSys/HotelBookingSysApplication.java
@@ -0,0 +1,13 @@
+package com.monohero.HotelBookingSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingSysApplication.class, args);
+	}
+
+}
